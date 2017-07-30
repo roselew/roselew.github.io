0ea98b4b552be0a067a739bd3bac2afbed2b0972
RLK <rozalewinska@gmail.com>
2017-07-31 00:50:50 +0200

    dolozona lapka w gore i dol, ale nadal mozna kliknac poza nimi

--- js/skrypt.js
+++ js/skrypt.js
@@ -24,14 +24,25 @@ $('.circle-mid').on('click', function(event){
 		$('.container').append($('<div class ="alert"> <span onclick="hideAlert()"> X </span> <p> Już wykonałeś ta misje </p> </div>'))
 	}
 	if (mission.parent('ul').hasClass('mission-undone')){
-		var WaitGroup = mission.closest('div').find('.mission-wait')
-		mission.appendTo(WaitGroup).hide().fadeIn('slow')
-		var smallMission = $(mission.closest('.one-full-day').prev().find('.small-mission-undone').children()[0])
-		var smallWaitGroup = smallMission.closest('div').find('.small-mission-wait')
-		smallMission.appendTo(smallWaitGroup).hide().fadeIn('slow')
+		mission.append($('<img src="assets/thumb_down.svg" class="thumb-down">'))
+		mission.append($('<img src="assets/thumb_up.svg" class="thumb-up">'))
+
+		$('img.thumb-down').on('click', function(ev){
+			ev.stopPropagation();
+			$('img.thumb-up,img.thumb-down').remove()
+		})
+
+		$('img.thumb-up').on('click', function(ev){
+			ev.stopPropagation();
+			$('img.thumb-up,img.thumb-down').remove()
+			fromUndoneToWait(mission)
+		})
+
 	}
 
 })
+
+
 
 // show alert
 function hideAlert() {
@@ -42,3 +53,27 @@ function hideAlert() {
 $(document).on('click',function(event){
 	console.log(event.target)
 })
+
+function fromUndoneToWait(mission) {
+	
+	var WaitGroup = mission.closest('div').find('.mission-wait')
+	mission.appendTo(WaitGroup).hide().fadeIn('slow')
+	var smallMission = $(mission.closest('.one-full-day').prev().find('.small-mission-undone').children()[0])
+	var smallWaitGroup = smallMission.closest('div').find('.small-mission-wait')
+	smallMission.appendTo(smallWaitGroup).hide().fadeIn('slow')
+	updateProgress();
+}
+	
+function updateProgress() {
+	var number_undone = $('.mission-undone .circle-mid').length
+	var number_wait = $('.mission-wait .circle-mid').length
+	var number_done = $('.mission-done .circle-mid').length
+	var number_total = number_undone + number_wait + number_done
+	$('.progress>p span').text(number_undone)
+	$('.progress-bar p').text(number_done + ' / ' + number_total)
+	$('.progress-bar .progress-wait').width(100*(number_done+number_wait)/number_total + '%');
+	$('.progress-bar .progress-done').width(100*number_done/number_total + '%');
+	if (number_undone == 0 & number_wait > 0) {
+		$('.progress>p').text('Wykonałeś wszystkie misje, czekamy tylko na akceptację mamy')
+	}
+}
