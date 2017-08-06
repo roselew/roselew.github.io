bffb02927fe85e20818ff4616ab79fa2602a3d98
RLK <rozalewinska@gmail.com>
2017-08-06 18:45:51 +0200

    schowane puste dni

--- js/skrypt.js
+++ js/skrypt.js
@@ -6,7 +6,8 @@ var color_base= '#00A6FF'; //blue
 var color_back_kid = '#0C1A3F'; 
 var color_back_parent= '#E6E8E5';
 
-// show full day
+// -------show day details-------------
+
 $('.day>p').on('click',function(event){
 	var full_day = $(this).parent('.day').children('.day-details')
 	if (full_day.is(":hidden")) {
@@ -20,70 +21,125 @@ $('.day>p').on('click',function(event){
 })
 
 
-// move mission between undone and wait 
-// generate alert if wait or done
+// -------moving misssions-------------
+
 $('.circle-mid').on('click', function(event){
-	var mission = $(this)
-	if (mission.parent('ul').hasClass('mission-wait')){
-		showAlert('Czekamy na akceptacje','wait');
-	}
-	if (mission.parent('ul').hasClass('mission-done')){
-		showAlert('Już wykonałeś ta misję','done');
-	}
-	if (mission.parent('ul').hasClass('mission-undone')){
-		$('.thumb-down').show().appendTo(mission)
-		$('.thumb-up').show().appendTo(mission)
-		showAlert('');
-		mission.css('z-index','500')
 
-		$('.thumb-down').on('click', function(ev){
-			ev.stopPropagation();
-			hideAlert();
-		})
+	var mission = $(this);
+	var missionType = mission.parents('ul').attr('class')
+	var kid_mode=mission.parents('body').hasClass('kid');
 
-		$('.thumb-up').on('click', function(ev){
-			ev.stopPropagation();
-			hideAlert();
-			fromUndoneToWait(mission)
-		})
+	//undone
+	if (missionType == 'mission-undone'){
+		if (kid_mode) {
+			showThumbs(mission,'wait','undone')
+		} else {
+			showThumbs(mission,'done','')
+		}
 	}
+
+	//wait
+	if (missionType =='mission-wait'){
+		if (kid_mode) {
+			showAlert('Czekamy na akceptacje','wait');
+		} else {
+			showThumbs(mission,'done','undone')
+		}
+	}
+
+	//done
+	if (missionType == 'mission-done'){
+		if (kid_mode){
+			showAlert('Już wykonałeś ta misję','done');
+
+		} else {
+			showThumbs(mission,'','undone')
+		}
+	}
+
 })
 
+
+function showThumbs(mission, stateYes, stateNo){
+
+	$('.thumb-down').show().appendTo(mission)
+	$('.thumb-up').show().appendTo(mission)
+	mission.css('z-index','500')
+
+	$('.thumb-up').on('click', function(ev){
+		ev.stopPropagation();
+		hideAlert();
+		if (stateYes){
+			moveMission(mission,stateYes)
+		}
+	})
+
+	$('.thumb-down').on('click', function(ev){
+		ev.stopPropagation();
+		hideAlert();
+		if (stateNo){
+			moveMission(mission,stateNo)
+		}
+	})
+
+}
+
+function moveMission(mission, state){
+	var missionType = mission.parents('ul').attr('class')
+	var midGroup=mission.parents('.day-details').find('.mission-'+state)
+	mission.appendTo(midGroup).hide().fadeIn('slow')
+	var smallMission = mission.parents('.day').children('.day-line').find(missionType).children().first()
+	var smallGroup = smallMission.parents('.day-line').find('.mission-'+state)
+	smallMission.appendTo(smallGroup).hide().fadeIn('slow')	
+	updateProgress();
+}
+
+function updateProgress() {
+	var number_undone = $('.mission-undone .circle-mid').length
+	var number_wait = $('.mission-wait .circle-mid').length
+	var number_done = $('.mission-done .circle-mid').length
+	var number_total = number_undone + number_wait + number_done
+	$('.progress-wait').width(100*(number_done+number_wait)/number_total + '%');
+	$('.progress-done').width(100*number_done/number_total + '%');
+	$('.show-all span').text(number_total)
+	$('.show-undone span').text(number_undone)
+	$('.show-wait span').text(number_wait)
+	$('.show-done span').text(number_done)
+}
+
+// -------filtering-------------
+
 $('.show-undone').on('click',function(){
-	showUndone();
+	showOnly('undone')
 })
 
 $('.show-wait').on('click',function(){
-	showWait();
+	showOnly('wait')
 })
 
 $('.show-done').on('click',function(){
-	showDone();
+	showOnly('done')
 })
 
 $('.show-all').on('click',function(){
 	showAll();
 })
 
-function showUndone() {
+function showOnly(type) {
 	hideAll()
-	$('.mission-undone').show()
-	$('.show-undone').css('background-color',color_undone)
-}
 
-function showWait() {
-	hideAll()
-	$('.mission-wait').show()
-	$('.show-wait').css('background-color',color_wait)
-}
+	$('.day').each(function(){
+		if ($(this).find('.mission-' + type + ' li').length) {
+		$(this).show()
+		}
+	})
+	$('.mission-' + type).show()
 
-function showDone() {
-	hideAll()	
-	$('.mission-done').show()
-	$('.show-done').css('background-color',color_done)
+	$('.show'+type).css('background-color',color_undone)
 }
 
 function showAll() {
+	$('.day').show()
 	$('.day-line').show()
 	$('.day-details').hide()
 	$('.mission-undone').show()		
@@ -94,6 +150,7 @@ function showAll() {
 }
 
 function hideAll(){
+	$('.day').hide()
 	$('.day-line').hide()
 	$('.day-details').show()
 	$('.mission-undone').hide()		
@@ -114,6 +171,9 @@ function deleteBackground(){
 	$('.kid .show-all').css('background-color',color_back_kid)
 }
 
+
+// -------alerts-------------
+
 function showAlert(message,type) {
 	$('.container').append($('<div class ="alert"> <span onclick="hideAlert()"> X </span> <p>' + message + '</p> </div>'))
 	if (type=="wait") {
@@ -128,8 +188,6 @@ function showAlert(message,type) {
  	 		width: "60%",
 		},1500)
 	}
-
-
 }
 
 function hideAlert() {
@@ -141,27 +199,7 @@ function hideAlert() {
 	},1500)
 }
 
-function fromUndoneToWait(mission) {
-	var waitGroup = mission.parents('.day-details').find('.mission-wait')
-	mission.appendTo(waitGroup).hide().fadeIn('slow')
-	var smallMission = mission.parents('.day').children('.day-line').find('.small-mission-undone').children().first()
-	var smallWaitGroup = smallMission.parents('.day-line').find('.small-mission-wait')
-	smallMission.appendTo(smallWaitGroup).hide().fadeIn('slow')
-	updateProgress();
-}
-	
-function updateProgress() {
-	var number_undone = $('.mission-undone .circle-mid').length
-	var number_wait = $('.mission-wait .circle-mid').length
-	var number_done = $('.mission-done .circle-mid').length
-	var number_total = number_undone + number_wait + number_done
-	$('.progress-wait').width(100*(number_done+number_wait)/number_total + '%');
-	$('.progress-done').width(100*number_done/number_total + '%');
-	$('.show-all span').text(number_total)
-	$('.show-undone span').text(number_undone)
-	$('.show-wait span').text(number_wait)
-	$('.show-done span').text(number_done)
-}
+
 
 // tymczasowe pokazuje gdzie klikamy
 $(document).on('click',function(event){
