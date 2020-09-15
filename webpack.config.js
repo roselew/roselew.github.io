1d1f7dfe646f3ecf7ac5830ff440c5e0d7eb6d04
roselew <rozalewinska@gmail.com>
2020-09-16 00:41:18 +0200

    Fix Scale Problem

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const { resolve } = require("path");
 const webpack = require("webpack");
 const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const src = (path) => resolve(__dirname, "src", path);
+const out = (path) => resolve(__dirname, "dist", path);
 
 module.exports = function (env) {
     var prod = env !== undefined && env.production === true;
@@ -10,13 +13,13 @@ module.exports = function (env) {
 
     return {
         entry: {
-            app: "./src/js/app.js",
+            main: src("js/main.js"),
         },
 
         output: {
-            // publicPath: dev ? "/dist/" : "",
-            path: resolve(__dirname, "dist/"),
-            filename: prod ? "[name].[chunkhash].js" : "[name].js",
+            path: out(""),
+            publicPath: "",
+            filename: prod ? "js/[name].[chunkhash].js" : "js/[name].js",
         },
 
         devtool: prod ? "source-map" : "cheap-module-eval-source-map",
@@ -57,14 +60,9 @@ module.exports = function (env) {
         plugins: [
             new ExtractTextWebpackPlugin("main.css"),
             new HtmlWebpackPlugin({
-                template: "./src/index.html",
+                template: src("index.html"),
             }),
-            // new CopyWebpackPlugin([
-            //     {
-            //         from: "src/images/",
-            //         to: "images/",
-            //     },
-            // ]),
+            new CopyWebpackPlugin([{ from: "src/images", to: "images" }]),
         ],
     };
 };
